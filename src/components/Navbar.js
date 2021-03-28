ce4f2ae6c109905cc1da72b54e196e591d00d61d
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-28 22:14:44 +0530

    styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,47 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom';
-import {ImBooks} from 'react-icons/im';
-import {FaBars,FaTimes} from 'react-icons/fa';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import { ImBooks } from 'react-icons/im';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css'
 
 function Navbar() {
-    const [click,setClick] =  useState(false)
+    const [click, setClick] = useState(false)
 
     const handleClick = () => setClick(!click)
-        return (
-            <>
-        <div className='navbar'>
-            <div className='container'>
-                <Link to='/' className='navbar-logo'>
-                    <ImBooks className='navbar-icon'/>
-                    AlViN EDU-Master
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    {click ?<FaTimes/> : <FaBars/>}
-                </div>
-                    <ul className={click? 'nav-menu active' : 'nav-menu'}>
+    return (
+        <>
+            <div className='navbar'>
+                <div className='container'>
+                    <Link to='/' className='navbar-logo'>
+                        <ImBooks className='navbar-icon' />
+                    <span className='header'>AlViN EDU-Master</span>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
-                        <Link to='/home' className='nav-links'>Home</Link>
-                        </li> 
+                            <Link to='/home' className='nav-links'>Home</Link>
+                        </li>
                         <li className='nav-item'>
-                        <Link to='/dashboard' className='nav-links'>Dashboard</Link>
-                        </li> 
+                            <Link to='/dashboard' className='nav-links'>Dashboard</Link>
+                        </li>
                         <li className='nav-item'>
-                        <Link to='/discussion' className='nav-links'>Discussion</Link>
-                        </li> 
+                            <Link to='/discussion' className='nav-links'>Discussion</Link>
+                        </li>
                         <li className='nav-item'>
-                        <Link to='/contact' className='nav-links'>Contact Us</Link>
-                        </li> 
+                            <Link to='/contact' className='nav-links'>Contact Us</Link>
+                        </li>
+                        <li className='nav-btn'>
+                            <Link to='/signin'>
+                                <Button variant="outline-info">Sign In</Button>
+                            </Link>
+                        </li>
                     </ul>
+                </div>
             </div>
-        </div>
         </>
     );
 }
